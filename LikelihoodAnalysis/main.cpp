f53e01f6f79db44ebb8ea9b923880b6235282ff4
Jonathan Miller <usmjonathanmiller@Jonathans-MacBook-Pro.local>
2015-07-10 15:02:49 -0400

    Initial Commit

---
+++ LikelihoodAnalysis/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LikelihoodAnalysis
+//
+//  Created by Jonathan Miller on 7/10/15.
+//  Copyright (c) 2015 Jonathan Miller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
